7587e5946fa6fd18f0e49eb3427f211f352539b3
moroix <freehear@gmail.com>
2024-05-15 13:48:37 +0100

    fixed search form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,14 +24,9 @@ const NavBar = ({ onSearch }) => {
     };
 
     return (
-<<<<<<< HEAD
-            <nav className='navbar-nav w-full bg-white shadow-md flex items-center justify-between px-4'>
-                {/* Logo */}
-=======
         <header>
             <nav className='navbar-nav w-full bg-white shadow-md flex items-center justify-between'>
                 
->>>>>>> af5a27a55986155ddcf12d0b48cce6a971ec9e69
                 <div className='flex justify-start'>
                     <a href='/'>
                         <Image
@@ -43,7 +38,6 @@ const NavBar = ({ onSearch }) => {
                         />
                     </a>
                 </div>
-<<<<<<< HEAD
                 {/* Bouton de recherche et barre de recherche */}
                 <div className='hidden lg:flex m-2'>
                         {/* <SearchBar onSearch={handleSearch} onInputChange={handleInputChange} /> */}
@@ -51,9 +45,6 @@ const NavBar = ({ onSearch }) => {
                 </div>
 
                 {/* Liens de navigation */}
-=======
-                
->>>>>>> af5a27a55986155ddcf12d0b48cce6a971ec9e69
                 <div className='hidden lg:flex'>
                     <ul className='flex gap-5'>
                         <li className='hover:border-b-2'><a href='/batiment'>Bâtiment</a></li>
@@ -62,7 +53,6 @@ const NavBar = ({ onSearch }) => {
                         <li className='hover:border-b-2'><a href='alimentation'>Alimentation</a></li>
                     </ul>
                 </div>
-<<<<<<< HEAD
                 {/* Menu déroulant pour les petits écrans */}
                 <button className='m-2 p-2 rounded bg-white hover:bg-slate-50 block cursor-pointer border lg:hidden' onClick={handleToggleClick}>
                         {/* Icône du bouton de recherche */}
@@ -79,43 +69,6 @@ const NavBar = ({ onSearch }) => {
                 </div>
                 )}
             </nav>
-=======
-                
-                <div className=' inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>          
-                    <button className='lg:hidden'
-                     onClick={handleToggleClick}>
-                    <svg
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        height="1em"
-                        width="1em"
-                    >
-                        <path d="M4 5h16a1 1 0 010 2H4a1 1 0 110-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2zm0 6h16a1 1 0 010 2H4a1 1 0 010-2z" />
-                    </svg>
-                    </button>
-                </div>
-                <div className='hidden lg:flex m-2'>
-                    <SearchBar onSearch={handleSearch} onInputChange={handleInputChange} />
-                </div>
-                
-            </nav>
-
-            {isClick && (
-                    <div className="navbar-nav w-full shadow-md items-center">
-                        <ul className='space-y-1 item-center px-2 pb-3 pt-2'>
-                            <li className='m-2'><a href='/batiment'>Bâtiment</a></li>
-                            <li className='m-2'><a href='/services'>Services</a></li>
-                            <li className='m-2'><a href='fabrication'>Fabrication</a></li>
-                            <li className='m-2'><a href='alimentation'>Alimentation</a></li>
-                        </ul>
-                        <div>
-                            <SearchBar/>
-                        </div>
-                    </div>
-                )}
-
-        </header>
->>>>>>> af5a27a55986155ddcf12d0b48cce6a971ec9e69
     );
 };
 
