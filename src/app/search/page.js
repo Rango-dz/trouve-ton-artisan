2ac24ccc81ec9de1cc831ef9e74242393a489ffa
moroix <freehear@gmail.com>
2024-05-17 22:09:21 +0100

    backend code finished

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,9 +1,12 @@
 'use client'
 import SearchForm from '@/components/SearchForm'
 import React from 'react'
+import { Suspense } from 'react'
 
 export default function page() {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <SearchForm />
+    </Suspense>
   )
 }
